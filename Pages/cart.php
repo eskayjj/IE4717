eaee4204bb8569bb17edd990c99b60a24be3d7b6
gniyihs <143016203+gniyihs@users.noreply.github.com>
2023-10-31 00:14:35 +0800

    Add files via upload

--- Pages/cart.php
+++ Pages/cart.php
@@ -6,6 +6,7 @@
     <title>Food Project Placeholder-Cart</title>
     <link rel="stylesheet" type="text/css" href="../Stylesheets/global.css">
     <link rel="stylesheet" type="text/css" href="../Stylesheets/cart.css">
+    <link rel="stylesheet" type="text/css" href="../Stylesheets/cartx.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Capriola&family=Inder&display=swap" rel="stylesheet">
@@ -19,7 +20,33 @@
         <?php
                 include 'loginheader.php';
         ?>
+
+        <div id="mycartTitle">
+            My Cart (X)
+        </div>
     </header>
+     <div class="content">   
+      <div class="canteenLabel">
+        <label>Canteen [?]</label>
+      </div>  
+       <div id="orderbox">
+       <label class="container">
+        <input type="checkbox">
+        <span class="checkmark"></span>
+       </label>
+        <img src="placeholder.jpg" alt="Image of Food" width="500" height="300" style="float: left; padding-left: 10%; padding-top: 6%;">
+        <div id="description">
+           <p> [Name of food] </p> <br><br> 
+           <p> [Item Description] </p> <br><br>
+           <p> [Qty] </p> <br><br>
+           <p> [Total Price] </p> <br><br>
+        </div>     
+       </div>
+       <div>
+       <p id="totalP" style="float: right;">Total: $X.XX</p>
+       <input type="submit" id="orderSubmit" name="orderBtn" value="Checkout">
+       </div>
+    </div>
     <?php
         include 'footer.php';
     ?>
