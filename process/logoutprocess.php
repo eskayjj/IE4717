5170605485e483b4fe773a72a2569018af44603c
eskayjj <shawnkoh25@gmail.com>
2023-11-03 19:53:20 +0800

    session_destroy

--- process/logoutprocess.php
+++ process/logoutprocess.php
@@ -1,5 +1,13 @@
 <?php
-    setcookie("user", "", time() - 3600 ,"/");
+    $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie("user", "", time() - 3600,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
     // echo "Cookie " . $_COOKIE['user'] . " is deleted.";
     header('location: ../Pages/index.php');
 ?>
